8063d4c19caa7a9080abfd991a44bd79cc7c43e7
Thiago Gonzalez <thiagogamerhd@gmail.com>
2021-03-30 02:11:14 -0300

    Added function exit and return menu.

--- main.c
+++ main.c
@@ -18,9 +18,99 @@
 Sempre que o usuário escolher uma opção deve ter a possibilidade de voltar para a tela de menu, exceto quando escolher por sair do sistema.
 ------------------------------------------------------------------------------------------------------------------------------------
 */
+
 #include <stdio.h>
+#include <stdlib.h>
 #include <locale.h>
 
+
+//função menu
+void menu()
+{
+
+	int user_select = 0;
+
+
+	printf("-------------MENU PRINCIPAL-------------\n");
+	printf("\n");
+	printf("1- Cadastrar\n");
+	printf("2- Listar\n");
+	printf("3- Excluir\n");
+	printf("4- Alterar\n");
+	printf("5- Sobre nós\n");
+	printf("6- Sair\n ");
+	printf("\n");
+	printf("Escolha uma opção: ");
+	scanf("%d", &user_select);
+
+	switch (user_select)
+	{
+	case 1:
+		//função cadastrar
+		system("cls");
+		printf("Teste cad.");
+		break;
+	case 2:
+		//função listar
+		system("cls");
+		printf("Teste list.");
+		break;
+	case 3:
+		//função excluir
+		system("cls");
+		break;
+	case 4:
+		//função alterar
+		system("cls");
+		break;
+	case 5:
+		//função sobre_nós
+		system("cls");
+		sobre_nos();
+		voltarMenu();
+		break;
+	case 6:
+		//função sair
+		sair();
+		break;
+	default:
+		//caso o usuário digite uma opção inválida, o programa retornará ao menu.
+		system("cls");
+		printf("Opção inválida... Retornando ao menu principal.\n");
+		return menu();
+		break;
+	}
+}
+
+//Função para retornar o menu
+void voltarMenu()
+{
+	int returnMenu;
+	printf("------------------------------------------------------------------------------------------------");
+	printf("\nDigite 1 para retornar ao menu\n");
+	printf("\nDigite 2 para sair do sistema\n");
+	scanf("%d", &returnMenu);
+
+	if (returnMenu == 1)
+	{
+		system("cls");
+		return menu();
+	}
+	else if (returnMenu == 2)
+	{
+		sair();
+	}
+}
+
+//função sair
+void sair()
+{
+	system("cls");
+	printf("Sistema encerrado.");
+	exit(0);
+}
+
+//função sobre nós 
 void sobre_nos()
 {
 
@@ -36,63 +126,10 @@ void sobre_nos()
 	printf("\n");
 	printf("Thiago Tavares Gonzalez             Matrícula: 2020016146\n");
 	printf("\n");
-
-	
 }
-
-void menu()
-{
-
-	int user_select = 0;
-
-	printf("1- Cadastrar\n");
-	printf("2- Listar\n");
-	printf("3- Excluir\n");
-	printf("4- Alterar\n");
-	printf("5- Sobre nós\n");
-	printf("6- Sair\n ");
-
-	scanf("%d",&user_select);
-
-	switch (user_select)
-	{
-	case 1:
-		//função cadastrar
-		printf("Teste cad.");
-		//1- Cadastrar o usuário
-		//2- Retornar para o menu
-		break;
-	case 2:
-		printf("Teste list.");
-		//função listar
-		break;
-	case 3:
-		//função excluir
-		break;
-
-	case 4:
-		//função alterar
-		break;
-	case 5:
-		sobre_nos();
-		//função sobre_nós
-		break;
-
-	case 6:
-		//função sair
-
-		
-		break;
-
-	default:
-		break;
-	}
-}
-
-
 
 int main()
-{ 
+{
 	setlocale(LC_ALL, "Portuguese");
 
 	menu();
